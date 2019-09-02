00d7580ca2537d680fcbc941f860c793beef0db2
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-09-01 21:36:09 -0400

    refactored config/routes.rb to namespace, looks better

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
-  get 'home/about'
-  get 'home/lookup'
-  post 'home/lookup' => 'home/lookup'
-  get 'home/news'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :home do
+      get :about
+      get :lookup
+      get :news
+      post :lookup
+  end
+  root to: 'home#index'
 end
